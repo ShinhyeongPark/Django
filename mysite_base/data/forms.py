970ad1410a8480a14991cbd05330cbbae5e1ba80
ShinhyeongPark <orpsh1941@gmail.com>
2021-01-28 00:18:09 +0900

    os.system

---
+++ mysite_base/data/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+# class RefreshForm(forms.Form):
+#     update = forms.
+#     #search_word = forms.CharField(label='Search Keyword')
--- mysite_base/data/views.py
+++ mysite_base/data/views.py
@@ -2,13 +2,15 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView
 from django.views.generic.dates import MonthArchiveView, DayArchiveView, TodayArchiveView
 from django.views.generic.edit import FormView
+from django import forms
 
 from django.db.models import Q
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+#from .forms import RefreshForm
 from .models import IT
 from .models import SPORTS
 from .models import ECO
+import os
 
 class NewsITLV(ListView):
     model = IT
@@ -16,7 +18,6 @@ class NewsITLV(ListView):
 class NewsITDV(DetailView):
     model = IT
     
-
 class NewsECOLV(ListView):
     model = ECO
 
@@ -30,4 +31,31 @@ class NewsSPORTSLV(ListView):
 class NewsSPORTSDV(DetailView):
     model = SPORTS
 
-    
+class UpdateIT(FormView):
+    template_name = "data/it_list.html"
+    success_url = "data/it_list.html"
+
+    def form_valid(self, form):
+        os.chdir('/Users/etlaou/Downloads/WebProgramming/mysite_base/ITscrapper')
+        os.system('scrapy crawl ITscrapper')
+        return super().form_valid(form)
+
+
+class UpdateSPORTS(FormView):
+    template_name = "data/sports_list.html"
+    success_url = "data/sports_list.html"
+
+    def form_valid(self, form):
+        os.chdir('/Users/etlaou/Downloads/WebProgramming/mysite_base/Sportsscrapper')
+        os.system('scrapy crawl Sportsscrapper')
+        return super().form_valid(form)
+
+
+class UpdateECO(FormView):
+    template_name = "data/eco_list.html"
+    success_url = "data/eco_list.html"
+
+    def form_valid(self, form):
+        os.chdir('/Users/etlaou/Downloads/WebProgramming/mysite_base/scrapper')
+        os.system('scrapy crawl scrapper')
+        return super().form_valid(form)
